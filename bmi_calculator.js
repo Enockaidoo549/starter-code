ea9a1037269514d77b80eb81b023f5085d95549d
Enockaidoo549 <enock.aidoo@amalitech.org>
2023-08-24 13:41:02 -0700

    message board display results aactive Now left with css manupulations

--- bmi_calculator.js
+++ bmi_calculator.js
@@ -1,39 +1,36 @@
 function bmi_calculator() {
   let heightInput = document.querySelector('.height-input');
   let weightInput = document.querySelector('.weight-input');
-  let metricRadio = document.querySelector('.metric');
-  let imperialRadio = document.querySelector('.imperial');
+  let metricRadio = document.getElementById('metric');
+  let imperialRadio = document.getElementById('imperial');
 
-  heightInput.addEventListener('input', calculateBMI);
-  weightInput.addEventListener('input', calculateBMI);
+  metricRadio.checked = true;
 
-  imperialRadio.addEventListener('click', function() {
-    metricRadio.disabled = true;
-  });
+  heightInput.addEventListener('input', calculateBMIWithDelay);
+  weightInput.addEventListener('input', calculateBMIWithDelay);
 
   metricRadio.addEventListener('click', function() {
-    imperialRadio.disabled = true;
+    metricRadio.checked = true;
+    imperialRadio.checked = false;
+    heightInput.placeholder = '0';
+    weightInput.placeholder = '0';
+    document.querySelector('.metric-height').textContent = 'cm';
+    document.querySelector('.metric-weight').textContent = 'kg';
   });
+
+  imperialRadio.addEventListener('click', function() {
+    imperialRadio.checked = true;
+    metricRadio.checked = false;
+    heightInput.placeholder = '0';
+    weightInput.placeholder = '0';
+    document.querySelector('.metric-height').textContent = 'in';
+    document.querySelector('.metric-weight').textContent = 'lbs';
+  });
+
+  heightInput.addEventListener('input', calculateBMIWithDelay);
+  weightInput.addEventListener('input', calculateBMIWithDelay);
 }
 
-function calculateBMI() {
-  let heightInput = document.querySelector('.height-input');
-  let weightInput = document.querySelector('.weight-input');
-
-  let height = parseFloat(heightInput.value);
-  let weight = parseFloat(weightInput.value);
-
-  if (isNaN(height) || isNaN(weight) || height <= 0 || weight <= 0) {
-    console.log("Invalid input values");
-    return;
-  }
-
-  let bmi = weight / (height * height);
-
-  console.log("Height:", height);
-  console.log("Weight:", weight);
-  console.log("BMI:", bmi);
-}
 function calculateBMIWithDelay() {
   let height = parseFloat(document.querySelector('.height-input').value);
   let weight = parseFloat(document.querySelector('.weight-input').value);
@@ -46,11 +43,19 @@ function calculateBMIWithDelay() {
     // Set a new timeout of 10 seconds to calculate and display the result
     window.bmiTimeout = setTimeout(function() {
       let bmi = weight / ((height / 100) * (height / 100));
+      let classification = " ";
+      if (bmi < 18.5){
+        classification="underweight";
+      } else if (bmi >= 18.5 && bmi <=24.9) {
+        classification = "Healthy weight";
+      }
       let message = `Your BMI is: ${bmi.toFixed(2)}`;
+      document.querySelector('.message-topic').textContent = message;
       console.log(message);
     }, 10000); // Delay of 10 seconds (10000 milliseconds)
+
+
   }
 }
-
 
 bmi_calculator();
